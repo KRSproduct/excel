162afb93cffb95266cb40d9da60a6f3b88f082de
KRSprod <krs@MacBook-Pro-2.local>
2022-11-17 00:43:24 +0200

    push latest changes

---
+++ src/redux/actions.js
@@ -0,0 +1,22 @@
+import { CHANGE_STYLES, CHANGE_TEXT, TABLE_RESIZE } from './types';
+
+export function tableResize(data) {
+	return {
+		type: TABLE_RESIZE,
+		data,
+	};
+}
+
+export function changeText(data) {
+	return {
+		type: CHANGE_TEXT,
+		data,
+	};
+}
+
+export function changeStyles(data) {
+	return {
+		type: CHANGE_STYLES,
+		data,
+	};
+}
